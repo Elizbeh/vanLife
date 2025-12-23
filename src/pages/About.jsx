1346e92d7b4eaf7e56991e089d92359a6f5e3bad
Elizbeh <105496336+Elizbeh@users.noreply.github.com>
2025-12-23 10:44:54 +0100

    chore: minor About page styling tweaks

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ function About() {
     <div className='about-container'>
       <img className='about-img' src={AboutImg} alt="Hero image" />
       <div className='about-text'>
-        <h1 about-title>Don’t squeeze in a sedan when you could relax in a van.</h1>
+        <h1 className='about-title'about-title>Don’t squeeze in a sedan when you could relax in a van.</h1>
         <p className='about-mission'>Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch.
           (Hitch costs extra 😉)</p>
         <p className='team-mission'>Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.</p>
